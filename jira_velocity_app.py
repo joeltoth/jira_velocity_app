659dc1faf68b5e14aaacf7f3b2a86d711939675d
Joel Toth <joeltoth@gmail.com>
2025-05-02 12:30:13 -0600

    Add files via upload

--- jira_velocity_app.py
+++ jira_velocity_app.py
@@ -44,7 +44,7 @@ def decrypt_api_token(token: str) -> str:
 
 # --- Auth setup ---
 DB_NAME = "users.db"
-REGISTRATION_CODE = "YOUR_REGISTRATION_CODE"
+REGISTRATION_CODE = st.secrets["REGISTRATION_CODE"]
 
 
 def init_db():
